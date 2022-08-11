db9604c757fa7ed7f2772f10e38c251e57d206ba
xiaochengfu <1033426413@qq.com>
2022-08-12 01:17:24 +0800

    feat: volatile的使用-可见性测试

---
+++ src/test/java/com/xiaochengfu/springbootstudy/VolatileTests.java
@@ -0,0 +1,42 @@
+package com.xiaochengfu.springbootstudy;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class VolatileTests {
+
+     private volatile int num = 0;
+
+    /**
+     * 知识点：
+     * 1. volatile 可见性,参考链接：https://blog.csdn.net/xueping_wu/article/details/124541419
+     * 2. 线程的使用new Thread().start();
+     * 3. 挂起线程的使用,包java.util.concurrent.TimeUnit
+     *
+     */
+    @Test
+    public void visible() {
+        new Thread(
+                () -> {
+                    try {
+                        // Thread.sleep(1000);也可以
+                        TimeUnit.SECONDS.sleep(1);//暂停线程
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    add10();
+                    System.out.println("主线程变量值未改变,等待..."+this.num);
+                }
+        ).start();
+
+        while (this.num==0){
+            //可以添加打印后，不加volatile确出现了可见性的情况
+        }
+        System.out.println("子线程内的内存变量->主内存通知主线程"+this.num);
+    }
+
+    private void add10() {
+        this.num +=10;
+    }
+}
